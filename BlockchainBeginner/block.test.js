c6339f1031ee02bd802d064af0714280d406f4c9
Shakthi <97010735+shakthii4@users.noreply.github.com>
2022-09-22 12:51:02 +0000

    'Genesis Block'

--- BlockchainBeginner/block.test.js
+++ BlockchainBeginner/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
 describe('Block', () => {
   const timestamp = 'a-date';
   const lastHash = 'foo-lasthash';
@@ -11,6 +12,20 @@ describe('Block', () => {
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+  });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+    console.log('genesisBlock', genesisBlock);
+
+    it('return a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+
+    });
+
   });
 
 });
---
+++ BlockchainBeginner/config.js
@@ -0,0 +1,7 @@
+const GENESIS_DATA={
+    timestamp:1,
+    lastHash:'-----',
+    hash:'hashOne',
+    data:[]
+};
+module.exports={GENESIS_DATA};
