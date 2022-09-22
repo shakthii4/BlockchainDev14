ac28af970de2c2d8d30da680ada3e1a5ef81cc2f
Shakthi <97010735+shakthii4@users.noreply.github.com>
2022-09-22 06:49:35 +0000

    'Create Block'

---
+++ BlockchainBeginner/block.js
@@ -0,0 +1,13 @@
+class Block{
+    constructor(timestamp,lastHash,hash,data)
+    {
+        this.timestamp=timestamp;
+        this.lastHash=lastHash;
+        this.hash=hash;
+        this.data=data;
+    }
+
+}
+
+const block1=new Block('01/01/01','foo-lasthash','foo-hash','foo-data');
+console.log(block1);
